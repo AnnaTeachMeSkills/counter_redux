5a74f6f782a75a726a77de71fa46e1b322e4e93d
unknown <khomich10@mail.ru>
2020-06-24 20:39:22 +0300

    yes

--- src/reducer.js
+++ src/reducer.js
@@ -1,7 +1,7 @@
 const reducer = (state = 0, action) => {
     switch (action.type) {
       case "INC": 
-        return state + 2;
+        return state + 1;
       case "DEC": 
         return state - 1;
       case "RND": 
